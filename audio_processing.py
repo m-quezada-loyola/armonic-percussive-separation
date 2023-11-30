e86654e6ea6a0a959b03360dcba92d8996be1bc3
Matias Quezada <m.quezada.loyola@gmail.com>
2023-11-30 00:50:47 -0300

    Add median filter functions

--- audio_processing.py
+++ audio_processing.py
@@ -1,5 +1,6 @@
 import numpy as np
 import librosa
+import scipy.signal as signal
 
 def calculate_spectrogram(data, frame_length: int, hop_size: int):
 
@@ -9,3 +10,13 @@ def calculate_spectrogram(data, frame_length: int, hop_size: int):
 def calculate_power_spectrogram(spectrogram):
 
     return np.abs(spectrogram) ** 2
+
+def vertical_median_filter(data, length):
+
+    filter_data = signal.medfilt(data, [length, 1])
+    return filter_data
+
+def horizontal_median_filter(data, length):
+
+    filter_data = signal.medfilt(data, [1, length])
+    return filter_data
