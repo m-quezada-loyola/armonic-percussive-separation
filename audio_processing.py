0967b90b7de74d7e4191bee8138d38ca8f2550b1
Matias Quezada <m.quezada.loyola@gmail.com>
2023-11-30 12:13:43 -0300

    Add function to create binary masks for harmonic, percussive and residual components

--- audio_processing.py
+++ audio_processing.py
@@ -21,3 +21,9 @@ def horizontal_median_filter(power_spectrogram, length):
     filtered_spectrogram = signal.medfilt(power_spectrogram, [1, length])
     return filtered_spectrogram
 
+def create_binary_masks(harmonic_spectrogram, percussive_spectrogram, beta):
+
+    harmonic_mask   = np.int8(harmonic_spectrogram >= beta * percussive_spectrogram)
+    percussive_mask = np.int8(percussive_spectrogram > beta * harmonic_spectrogram)
+    residual_mask   = 1 - (harmonic_mask + percussive_mask)
+    return harmonic_mask, percussive_mask, residual_mask
