d1b6c21e5e4b59cc5bb5b3296f2dc36eba911ff1
Matias Quezada <m.quezada.loyola@gmail.com>
2023-12-29 18:23:45 -0300

    Create audio source separation function with harmonic, percussive and residual components

--- audio_processing.py
+++ audio_processing.py
@@ -56,3 +56,36 @@ def generate_audio_component(spectrogram, component_mask, frame_length, hop_size
     audio_component = recover_audio(component_spectrogram, frame_length, hop_size, data_length)
     return audio_component, component_spectrogram
 
+
+def generate_hprs(data, fs, frame_length, hop_size, beta, harmonic_median_length, percussive_median_length):
+
+    median_length_frames = even_to_odd(calculate_frame_length(harmonic_median_length, fs, hop_size))
+    median_length_bins = even_to_odd(calculate_bin_length(percussive_median_length, fs, frame_length))
+
+    spectrogram = calculate_spectrogram(data, frame_length, hop_size)
+    power_spectrogram = calculate_power_spectrogram(spectrogram)
+    harmonic_power_spectrogram = horizontal_median_filter(power_spectrogram, median_length_frames)
+    percussive_power_spectrogram = vertical_median_filter(power_spectrogram, median_length_bins)
+
+    harmonic_mask = create_harmonic_mask(harmonic_power_spectrogram, percussive_power_spectrogram, beta)
+    percussive_mask = create_percussive_mask(harmonic_power_spectrogram, percussive_power_spectrogram, beta)
+    residual_mask = create_residual_mask(harmonic_mask, percussive_mask)
+
+    harmonic_audio, harmonic_spectrogram = generate_audio_component(spectrogram, harmonic_mask, frame_length, hop_size, data.size)
+    percussive_audio, percussive_spectrogram = generate_audio_component(spectrogram, percussive_mask, frame_length, hop_size, data.size)
+    residual_audio, residual_spectrogram = generate_audio_component(spectrogram, residual_mask, frame_length, hop_size, data.size)
+
+    audios = {
+        'harmonic': harmonic_audio, 'percussive': percussive_audio, 'residual': residual_audio
+    }
+
+    masks = {
+        'harmonic': harmonic_mask, 'percussive': percussive_mask, 'residual': residual_mask
+    }
+
+    spectrograms = {
+        'original': spectrogram, 'harmonic': harmonic_spectrogram,
+        'percussive': percussive_spectrogram, 'residual': residual_spectrogram
+    }
+
+    return audios, masks, spectrograms
